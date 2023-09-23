8274628b80355db7bd6878150d1f1c26a93ccd7b
maddie <lee.maddie@gmail.com>
2023-09-23 18:14:44 -0400

    Refactored function to follow assignment instructions

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,18 +3,18 @@ const request = require('request');
 // Allow the user to specify breed name using command line
 const breed = process.argv[2];
 
-const fetchBreedDescription = (breed) => {
-  request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
     if (error) {
-      console.log(error);
+      callback(error);
     } else {
       const data = JSON.parse(body);
 
       if (data.length === 0) {
         console.log('Breed not found!');
       } else {
-        console.log(data[0].description);
+        callback(data[0].description);
       }
     }
-  })
+  });
 };
