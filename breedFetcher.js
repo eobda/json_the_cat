a76cf01123a0cca5b393d2dd53dd50394d883ff7
maddie <lee.maddie@gmail.com>
2023-09-23 17:51:25 -0400

    Changed query to use dynamic breed name from command line argument

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,9 @@
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
+// Allow the user to specify breed name using command line
+const breed = process.argv[2];
+
+request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
   const data = JSON.parse(body);
 
   console.log(data[0].description);
