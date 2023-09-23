4eea71c6dcd7d02c79a1caa0ecbb4aba4e23b8a8
maddie <lee.maddie@gmail.com>
2023-09-23 17:48:02 -0400

    Added JSON.parse to convert data received by request

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,7 @@
 const request = require('request');
 
 request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
-  console.log(body);
-  console.log('typeof body:', typeof body);
+  const data = JSON.parse(body);
+  console.log(data);
+  console.log('typeof data:', typeof data);
 });
