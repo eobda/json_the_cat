e44441711b3e2f12950fc634c3d7dc0dfa2310fb
maddie <lee.maddie@gmail.com>
2023-09-23 17:46:43 -0400

    Added request callback with API

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,6 @@
 const request = require('request');
+
+request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
+  console.log(body);
+  console.log('typeof body:', typeof body);
+});
