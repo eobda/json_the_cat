bd3d434e21a0203ccd1187f7825a877abf0f51c2
maddie <lee.maddie@gmail.com>
2023-09-23 18:24:38 -0400

    Refactored code to match callback in index

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,14 +3,14 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
     if (error) {
-      callback(error);
+      callback(error, null);
     } else {
       const data = JSON.parse(body);
 
       if (data.length === 0) {
-        console.log('Breed not found!');
+        callback(null, 'Breed not found!');
       } else {
-        callback(data[0].description);
+        callback(null, data[0].description);
       }
     }
   });
--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const { fetchBreedDescription } = require('./breedFetcher');
 
 // Allow the user to specify breed name using command line
 const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
