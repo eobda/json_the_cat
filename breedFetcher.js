c406cb470e2150d217dd224243d8e2550e8d0656
maddie <lee.maddie@gmail.com>
2023-09-23 18:07:13 -0400

    Added error handler and logging error details to the screen

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,11 +4,15 @@ const request = require('request');
 const breed = process.argv[2];
 
 request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
-  const data = JSON.parse(body);
-
-  if (data.length === 0) {
-    console.log('Breed not found!');
+  if (error) {
+    console.log(error);
   } else {
-    console.log(data[0].description);
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      console.log('Breed not found!');
+    } else {
+      console.log(data[0].description);
+    }
   }
 });
